c6acbc2c6189564d5ab7484b02687cd6ebfaf538
Edalik <www.ayakubov@yandex.ru>
2025-03-08 21:31:23 +0300

    initial commit

---
+++ init-scripts/create-schemas.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA users;
+CREATE USER user_service WITH PASSWORD 'user_service';
+GRANT ALL PRIVILEGES ON SCHEMA users TO user_service;
+
+CREATE SCHEMA products;
+CREATE USER product_service WITH PASSWORD 'product_service';
+GRANT ALL PRIVILEGES ON SCHEMA products TO product_service;
+
+CREATE SCHEMA notifications;
+CREATE USER notification_service WITH PASSWORD 'notification_service';
+GRANT ALL PRIVILEGES ON SCHEMA notifications TO notification_service;
